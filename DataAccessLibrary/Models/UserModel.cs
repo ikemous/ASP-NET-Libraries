7eb17db117edf4eff09694eafb27953bf1c7c2db
Ikemous <barranco.ike@outlook.com>
2022-11-25 22:11:31 -0700

    added Data Access Library

---
+++ DataAccessLibrary/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
---
+++ DataAccessLibrary/UserData.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _db;
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public Task<List<UserModel>> GetUsers()
+        {
+            string sql = "SELECT * FROM Users";
+            return _db.LoadData<UserModel, dynamic>(sql, new { });
+        }
+        public Task InsertUser(UserModel user)
+        {
+            string sql = @"INSERT INTO Users (UserEmail, UserPassword)
+                           VALUES (@UserEmail, @UserPassword);";
+            return _db.SaveData(sql, user);
+        }
+    }
+}
